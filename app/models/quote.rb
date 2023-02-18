360ae94cf8f158c11a18fa295e98f74f65aa3bae
Sameer Qaiser <iamsamkh@gmail.com>
2023-02-19 01:21:18 +0500

    added action cable settings

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,5 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: {quote: self}, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" } # broadcast_prepend_to
+  # after_update_commit -> { broadcast_replace_later_to "quotes" } # broadcast_replace_to
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Similar Functionality
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
