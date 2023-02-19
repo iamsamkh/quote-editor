f8cb012475f017a9b78b1d79f2aafd26af2d0ad4
Sameer Qaiser <iamsamkh@gmail.com>
2023-02-19 23:30:32 +0500

    added line items and more tweeks

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, expect: [:index, :show]
+    resources :line_item_dates, expect: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
