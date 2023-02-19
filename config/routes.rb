6099a735dc38463d756a11fb739eb80581e9d38d
Sameer Qaiser <iamsamkh@gmail.com>
2023-02-19 20:46:39 +0500

    added line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   devise_for :users
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, expect: [:index, :show]
+  end
 end
